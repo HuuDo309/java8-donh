ec7d15967358be1d8e368fa4534aa0b3b3e0b570
HuuDo309 <huudo1509665@gmail.com>
2024-12-16 17:12:35 +0700

    update controller

---
+++ src/main/java/com/digidinos/shopping/controller/MainController.java
@@ -0,0 +1,267 @@
+package com.digidinos.shopping.controller;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.digidinos.shopping.dao.OrderDAO;
+import com.digidinos.shopping.dao.ProductDAO;
+import com.digidinos.shopping.entity.Product;
+import com.digidinos.shopping.form.CustomerForm;
+import com.digidinos.shopping.model.CartInfo;
+import com.digidinos.shopping.model.CustomerInfo;
+import com.digidinos.shopping.model.ProductInfo;
+import com.digidinos.shopping.pagination.PaginationResult;
+import com.digidinos.shopping.utils.Utils;
+import com.digidinos.shopping.validator.CustomerFormValidator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@Controller
+@Transactional
+public class MainController {
+
+	@Autowired
+	private OrderDAO orderDAO;
+
+	@Autowired
+	private ProductDAO productDAO;
+
+	@Autowired
+	private CustomerFormValidator customerFormValidator;
+
+	@InitBinder
+	public void myInitBinder(WebDataBinder dataBinder) {
+		Object target = dataBinder.getTarget();
+		if (target == null) {
+			return;
+		}
+		System.out.println("Target=" + target);
+
+		// Trường hợp update SL trên giỏ hàng.
+		// (@ModelAttribute("cartForm") @Validated CartInfo cartForm)
+		if (target.getClass() == CartInfo.class) {
+
+		}
+
+		// Trường hợp save thông tin khách hàng.
+		// (@ModelAttribute @Validated CustomerInfo customerForm)
+		else if (target.getClass() == CustomerForm.class) {
+			dataBinder.setValidator(customerFormValidator);
+		}
+
+	}
+
+	@GetMapping("/403")
+	public String accessDenied() {
+		return "/403";
+	}
+
+	@GetMapping("/")
+	public String home() {
+		return "index";
+	}
+
+	// Danh sách sản phẩm.
+	@GetMapping({ "/productList" })
+	public String listProductHandler(Model model, //
+			@RequestParam(value = "name", defaultValue = "") String likeName,
+			@RequestParam(value = "page", defaultValue = "1") int page) {
+		final int maxResult = 5;
+		final int maxNavigationPage = 10;
+
+		PaginationResult<ProductInfo> result = productDAO.queryProducts(page, //
+				maxResult, maxNavigationPage, likeName);
+
+		model.addAttribute("paginationProducts", result);
+		return "productList";
+	}
+
+	// GET: Thêm sản phẩm vào giỏ hàng theo mã (code)
+	@GetMapping({ "/buyProduct" })
+	public String listProductHandler(HttpServletRequest request, Model model, //
+			@RequestParam(value = "code", defaultValue = "") String code) {
+
+		Product product = null;
+		if (code != null && code.length() > 0) {
+			product = productDAO.findProduct(code);
+		}
+		if (product != null) {
+
+			// 
+			CartInfo cartInfo = Utils.getCartInSession(request);
+
+			ProductInfo productInfo = new ProductInfo(product);
+
+			cartInfo.addProduct(productInfo, 1);
+		}
+
+		return "redirect:/shoppingCart";
+	}
+
+	// GET: Xóa sản phẩm khỏi giỏ hàng theo mã (code)
+	@GetMapping({ "/shoppingCartRemoveProduct" })
+	public String removeProductHandler(HttpServletRequest request, Model model, //
+			@RequestParam(value = "code", defaultValue = "") String code) {
+		Product product = null;
+		if (code != null && code.length() > 0) {
+			product = productDAO.findProduct(code);
+		}
+		if (product != null) {
+
+			CartInfo cartInfo = Utils.getCartInSession(request);
+
+			ProductInfo productInfo = new ProductInfo(product);
+
+			cartInfo.removeProduct(productInfo);
+
+		}
+
+		return "redirect:/shoppingCart";
+	}
+
+	// POST: Cập nhập số lượng cho các sản phẩm đã mua.
+	@PostMapping({ "/shoppingCart" })
+	public String shoppingCartUpdateQty(HttpServletRequest request, //
+			Model model, //
+			@ModelAttribute("cartForm") CartInfo cartForm) {
+
+		CartInfo cartInfo = Utils.getCartInSession(request);
+		cartInfo.updateQuantity(cartForm);
+
+		return "redirect:/shoppingCart";
+	}
+
+	// GET: Hiển thị giỏ hàng.
+	@GetMapping({ "/shoppingCart" })
+	public String shoppingCartHandler(HttpServletRequest request, Model model) {
+		CartInfo myCart = Utils.getCartInSession(request);
+
+		model.addAttribute("cartForm", myCart);
+		return "shoppingCart";
+	}
+
+	// GET: Nhập thông tin khách hàng.
+	@GetMapping({ "/shoppingCartCustomer" })
+	public String shoppingCartCustomerForm(HttpServletRequest request, Model model) {
+
+		CartInfo cartInfo = Utils.getCartInSession(request);
+
+		if (cartInfo.isEmpty()) {
+
+			return "redirect:/shoppingCart";
+		}
+		CustomerInfo customerInfo = cartInfo.getCustomerInfo();
+
+		CustomerForm customerForm = new CustomerForm(customerInfo);
+
+		model.addAttribute("customerForm", customerForm);
+
+		return "shoppingCartCustomer";
+	}
+
+	// POST: Save thông tin khách hàng.
+	@PostMapping({ "/shoppingCartCustomer" })
+	public String shoppingCartCustomerSave(HttpServletRequest request, //
+			Model model, //
+			@ModelAttribute("customerForm") @Validated CustomerForm customerForm, //
+			BindingResult result, //
+			final RedirectAttributes redirectAttributes) {
+
+		if (result.hasErrors()) {
+			customerForm.setValid(false);
+			// Forward tới trang nhập lại.
+			return "shoppingCartCustomer";
+		}
+
+		customerForm.setValid(true);
+		CartInfo cartInfo = Utils.getCartInSession(request);
+		CustomerInfo customerInfo = new CustomerInfo(customerForm);
+		cartInfo.setCustomerInfo(customerInfo);
+
+		return "redirect:/shoppingCartConfirmation";
+	}
+
+	// GET: Xem lại thông tin để xác nhận.
+	@GetMapping({ "/shoppingCartConfirmation" })
+	public String shoppingCartConfirmationReview(HttpServletRequest request, Model model) {
+		CartInfo cartInfo = Utils.getCartInSession(request);
+
+		if (cartInfo == null || cartInfo.isEmpty()) {
+
+			return "redirect:/shoppingCart";
+		} else if (!cartInfo.isValidCustomer()) {
+
+			return "redirect:/shoppingCartCustomer";
+		}
+		model.addAttribute("myCart", cartInfo);
+
+		return "shoppingCartConfirmation";
+	}
+
+	// POST: Gửi đơn hàng (Save).
+	@PostMapping({ "/shoppingCartConfirmation" })
+
+	public String shoppingCartConfirmationSave(HttpServletRequest request, Model model) {
+		CartInfo cartInfo = Utils.getCartInSession(request);
+
+		if (cartInfo.isEmpty()) {
+
+			return "redirect:/shoppingCart";
+		} else if (!cartInfo.isValidCustomer()) {
+
+			return "redirect:/shoppingCartCustomer";
+		}
+		try {
+			orderDAO.saveOrder(cartInfo);
+		} catch (Exception e) {
+
+			return "shoppingCartConfirmation";
+		}
+
+		// Xóa giỏ hàng khỏi session.
+		Utils.removeCartInSession(request);
+
+		// Lưu thông tin đơn hàng cuối đã xác nhận mua.
+		Utils.storeLastOrderedCartInSession(request, cartInfo);
+
+		return "redirect:/shoppingCartFinalize";
+	}
+
+	// GET: Thông tin đơn hàng cuối cùng 
+	@GetMapping({ "/shoppingCartFinalize" })
+	public String shoppingCartFinalize(HttpServletRequest request, Model model) {
+
+		CartInfo lastOrderedCart = Utils.getLastOrderedCartInSession(request);
+
+		if (lastOrderedCart == null) {
+			return "redirect:/shoppingCart";
+		}
+		model.addAttribute("lastOrderedCart", lastOrderedCart);
+		return "shoppingCartFinalize";
+	}
+
+	// GET: Hiển thị hình ảnh sản phẩm theo mã (code)
+	@GetMapping({ "/productImage" })
+	public void productImage(HttpServletRequest request, HttpServletResponse response, Model model,
+			@RequestParam("code") String code) throws IOException {
+		Product product = null;
+		if (code != null) {
+			product = this.productDAO.findProduct(code);
+		}
+		if (product != null && product.getImage() != null) {
+			response.setContentType("image/jpeg, image/jpg, image/png, image/gif");
+			response.getOutputStream().write(product.getImage());
+		}
+		response.getOutputStream().close();
+	}
+
+}
--- src/main/java/com/digidinos/shopping/dao/ProductDAO.java
+++ src/main/java/com/digidinos/shopping/dao/ProductDAO.java
@@ -11,6 +11,7 @@ import org.hibernate.query.Query;
 import com.digidinos.shopping.entity.Product;
 import com.digidinos.shopping.form.ProductForm;
 import com.digidinos.shopping.model.ProductInfo;
+import com.digidinos.shopping.pagination.PaginationResult;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -27,7 +28,7 @@ public class ProductDAO {
 	
 	public Product findProduct(String code) {
 		try {
-			String sql = "Select from " + Product.class.getName() + "e Where e.code = :code";
+			String sql = "Select e from " + Product.class.getName() + " e Where e.code = :code";
 			
 			Session session = this.sessionFactory.getCurrentSession();
 			Query<Product> query = session.createQuery(sql, Product.class);
@@ -86,4 +87,26 @@ public class ProductDAO {
 		session.flush();
 	}
 
+	public PaginationResult<ProductInfo> queryProducts(int page, int maxResult, int maxNavigationPage, String likeName) {
+		String sql = "Select new " + ProductInfo.class.getName() //
+				+ "(p.code, p.name, p.price)" + " from " //
+				+ Product.class.getName() + " p ";
+		if (likeName != null && likeName.length() > 0) {
+			sql += "Where lower(p.name) like :likeName ";
+		}
+		sql += " order by p.createDate desc ";
+		//
+		Session session = this.sessionFactory.getCurrentSession();
+		Query<ProductInfo> query = session.createQuery(sql, ProductInfo.class);
+		
+		if (likeName != null && likeName.length() > 0) {
+			query.setParameter("likeName", "%" + likeName.toLowerCase() + "%");
+		}
+		return new PaginationResult<ProductInfo>(query, page, maxResult, maxNavigationPage);
+	}
+	
+	public PaginationResult<ProductInfo> queryProducts(int page, int maxResult, int maxNavigationPage) {
+		return queryProducts(page, maxResult, maxNavigationPage, null);
+	}
+	
 }
