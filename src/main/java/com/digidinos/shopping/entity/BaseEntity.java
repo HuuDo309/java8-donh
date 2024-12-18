553fa996cee2fdd5844ec76b0c94b36adc4ac20f
HuuDo309 <huudo1509665@gmail.com>
2024-12-18 15:59:43 +0700

    update

--- src/main/java/com/digidinos/shopping/entity/BaseEntity.java
+++ src/main/java/com/digidinos/shopping/entity/BaseEntity.java
@@ -7,36 +7,66 @@ import javax.persistence.Column;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 
-public class BaseEntity implements Serializable{
-	public static final long serialVersionUID = 1842915944086730414L;
-	
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "id", nullable = false, updatable = false)
-	protected String id;
-	
-	protected LocalDateTime createdAt;
-	protected LocalDateTime updatedAt;
-	protected LocalDateTime deletedAt;
-	private boolean isDeleted;
-	
-	protected BaseEntity(String id, LocalDateTime createdAt, LocalDateTime updatedAt, LocalDateTime deletedAt, boolean isDeleted) {
-		this.id =id;
-		this.createdAt = createdAt;
-		this.updatedAt = updatedAt;
-		this.deletedAt = deletedAt;
-		//this.setDeleted(isDeleted);
-	}
-	
-	public String getId() {
-		return id;
-	}
-	
-	public void setId(String id) {
-		this.id = id;
-	}
-	
+@MappedSuperclass
+public class BaseEntity implements Serializable {
 
-	
+    public static final long serialVersionUID = 1842915944086730414L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false, updatable = false)
+    protected String id;
+
+    protected LocalDateTime createdAt;
+    protected LocalDateTime updatedAt;
+    protected LocalDateTime deletedAt;
+    private boolean isDeleted;
+
+
+    public BaseEntity() {
+
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public LocalDateTime getDeletedAt() {
+        return deletedAt;
+    }
+
+    public void setDeletedAt(LocalDateTime deletedAt) {
+        this.deletedAt = deletedAt;
+    }
+
+    public boolean isDeleted() {
+        return isDeleted;
+    }
+
+    public void setDeleted(boolean isDeleted) {
+        this.isDeleted = isDeleted;
+    }
 }
+
